8c4d929bc13c6f37014c466c1b923e3c7788742a
joashgobin <joashgobin@gmail.com>
2025-07-27 22:10:03 -0400

    Added new functions to store and retrieve multiple values from shelf

--- helpers/shelf.go
+++ helpers/shelf.go
@@ -2,14 +2,18 @@ package helpers
 
 import (
 	"database/sql"
+	"fmt"
+	"strings"
+
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gofiber/fiber/v2/log"
-	"strings"
 )
 
 type ShelfModelInterface interface {
 	Set(key string, value string)
 	Get(key string) string
+	GetMany(filter string) map[string]string
+	SetMany(pairs map[string]string) error
 }
 
 type ShelfModel struct {
@@ -22,6 +26,52 @@ func (s *ShelfModel) Set(key string, value string) {
 
 func (s *ShelfModel) Get(key string) string {
 	return GetShelf(s.DB, key)
+}
+
+func (s *ShelfModel) GetMany(filter string) map[string]string {
+	query := `
+	SELECT name,value FROM shelf WHERE name LIKE '%` + filter + `%'
+	`
+	rows, err := s.DB.Query(query)
+	if err != nil {
+		return map[string]string{}
+	}
+	values := make(map[string]string)
+	for rows.Next() {
+		name := ""
+		value := ""
+		err = rows.Scan(&name, &value)
+		if err != nil {
+			log.Errorf("scan error: %v", err)
+		}
+		values[name] = value
+	}
+	return values
+}
+
+func (s *ShelfModel) SetMany(pairs map[string]string) error {
+	placeholders := make([]string, len(pairs))
+	values := make([]interface{}, 0)
+	count := 0
+	for i, pair := range pairs {
+		placeholders[count] = "(?, ?)"
+		count++
+		values = append(values, i, pair)
+	}
+	query := fmt.Sprintf(`
+		INSERT INTO shelf (name, value)
+		VALUES %s
+		ON DUPLICATE KEY UPDATE
+			value = VALUES(value)
+		`, strings.Join(placeholders, ","))
+	_, err := s.DB.Exec(query, values...)
+	// log.Infof("inserting multiple values:\n%v", query)
+	if err != nil {
+		log.Errorf("multiple insert error: %v", err)
+		return err
+	}
+	// log.Infof("updated key-value pairs: %v", pairs)
+	return nil
 }
 
 func SetShelf(db *sql.DB, key string, value string) {
--- logs/joashgobin-2025-07-27.md
+++ logs/joashgobin-2025-07-27.md
@@ -2,3 +2,4 @@
 
 - Added app reference to fiber storage in base app
 - Fixed error with Storage interface
+- Added new functions to store and retrieve multiple values from shelf
